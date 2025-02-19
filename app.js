1314ba1d4285c35d865fe42c032ca1105ce9cc18
unknown <jawad21ullah@gmail.com>
2025-02-19 19:02:20 +0500

    update

---
+++ app.js
@@ -0,0 +1,95 @@
+// USER INPUT & CONDITIONAL STATEMENT (start)
+// Q1
+// var cityName = prompt("Please enter your city name");
+// if(cityName.toLowerCase()=="karachi"){
+//     console.log("Welcome to city of lights")
+// }else{
+//     console.log("Welcome to",(cityName));
+// }
+
+// Q2
+// var gender = prompt("Enter your gender");
+// if(gender.toLowerCase()=="male"){
+//     console.log("Good Morning Sir.");
+// }else if(gender.toLowerCase()=="female"){
+//     console.log("Good Morning aunties.");
+// }else{
+//     console.log('Invalid input. Please enter male or female.');
+// }
+
+// Q3
+// var input = prompt('Enter traffic signal color').toLowerCase();
+// if(input == 'red'){
+//     alert(`${input}: Must Stop`);
+// } else if(input == 'yellow'){
+//     alert(`${input}: Ready to move`);
+// } else if(input == 'green'){
+//     alert(`${input}: Move now`);
+// }else alert('is not traffic color. traffic color is Red, Yellow, Green.');
+
+// Q4
+// var fuel = +prompt('Fuel remaining message');
+// var message;
+// if(fuel<=5){
+//     message ='refill the fuel in your car';
+// }else if(fuel>=6){
+//     message ='fulfill the fuel in your car';
+// }
+// console.log((message));
+
+// Q5
+// a)
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// b)
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// c)
+// var c = 12;
+// var message;
+// if (c++ === 11){
+//     message = "condition 1 is true";
+// }
+// else if (c === 12){
+//     message = "condition 2 is true";
+//     }
+// else if (++c < 14){
+//     message = "condition 3 is true";
+//     }
+// else if(c === 14){
+//     message = "condition 4 is true";
+// }
+// alert(message);
+
+// d)
+// var materialCost = 2000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// e)
+//  if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// f)
+//  if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// Q6
+
+// USER INPUT & CONDITIONAL STATEMENT (end)
+
+
